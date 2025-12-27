aa87530eaf9d55056bb1a1d6ff8db29169e2fab1
Pablo Andres Chacon <pabloandres120297@gmail.com>
2025-12-27 13:30:45 -0500

    add Shadcn and working on TaskApp

--- src/main.tsx
+++ src/main.tsx
@@ -6,13 +6,15 @@ import { createRoot } from 'react-dom/client'
 // import { TrafficLightWithEffect } from './02-useEffect/TrafficLightWithEffect'
 // import { TrafficLightWithHooks } from './02-useEffect/TrafficLightWithHooks'
 // import { PokemonPage } from './03-examples/PokemonPage'
-import { FocusScreen } from './hooks/FocusScreen'
+// import { FocusScreen } from './hooks/FocusScreen'
+import { TasksApp } from './05-useReducer/TaskApp'
 
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     {/* <TrafficLightWithEffect /> */}
-    <FocusScreen />
+    {/* <FocusScreen /> */}
+    <TasksApp />
   </StrictMode>,
 )
